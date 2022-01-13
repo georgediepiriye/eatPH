0eeeee4d0f5a5005cc966c9709db85d3da90501e
georgediepiriye <georgediepiriye4u@gmail.com>
2022-01-13 20:15:31 +0100

    added route to get all restaurants

--- server/server.js
+++ server/server.js
@@ -7,6 +7,22 @@ const app = express()
 
 app.use(morgan('dev'))
 
+//get all restaurants
+app.get("/api/v1/restaurants",async(req,res)=>{
+    try{
+        const results = await db.query("select * from restaurants;")
+        res.status(200).json({
+            status:'success',
+            results: results.rows.length,
+            data:{
+                restaurants: results.rows
+            }
+        })
+    
+    }catch(err){
+        console.log(err)
+    }
+})
 
 
 const PORT = process.env.PORT
